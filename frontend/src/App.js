4a7d13213418a92ed047e8b4e52001b0755ef93b
Basir <basir.jafarzadeh@gmail.com>
2020-01-30 23:36:57 +0330

    Video-11-Manage-State-With-Redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './App.css';
-import HomeScreen from './Screens/HomeScreen';
-import ProductScreen from './Screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
 
---
+++ frontend/src/store.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
+
+
+const initialState = {};
+const reducer = combineReducers({
+  productList: productListReducer,
+})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+export default store;
