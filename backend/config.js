bc8200e9ea1a12c9ef2ae086fd6194106c95edc2
Bassir JD (Programming Teacher) <basir.jafarzadeh@gmail.com>
2020-02-01 21:44:12 +0330

    Video-14-Connect-to-MongoDB (#12)

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export default {
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona'
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 import express from 'express';
 import data from './data';
+import dotenv from 'dotenv';
+import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).catch(error => console.log(error.reason));
+
 
 const app = express();
 
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
   const product = data.products.find(x => x._id === productId);
