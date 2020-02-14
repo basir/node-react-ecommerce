e755f84cba9fb2f2ef709b782f290f98db1b91a1
Basir <basir.jafarzadeh@gmail.com>
2020-02-14 10:10:05 +0330

    Video-23-Deploy-Website-on-Heroku

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,11 @@
 import express from 'express';
 import data from './data';
-import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
 import orderRoute from './routes/orderRoute';
-
-dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
