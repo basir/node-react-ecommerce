fde6bc1a07559f65a3fefe7f6c70807f5e0da568
Bassir JD (Programming Teacher) <basir.jafarzadeh@gmail.com>
2020-03-20 11:35:36 +0330

    Add ESLint (#22)

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import data from './data';
-import config from './config';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import data from './data';
+import config from './config';
 import userRoute from './routes/userRoute';
 import productRoute from './routes/productRoute';
 import orderRoute from './routes/orderRoute';
@@ -11,18 +11,18 @@ const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true
-}).catch(error => console.log(error.reason));
+  useCreateIndex: true,
+}).catch((error) => console.log(error.reason));
 
 
 const app = express();
 app.use(bodyParser.json());
-app.use("/api/users", userRoute);
-app.use("/api/products", productRoute);
-app.use("/api/orders", orderRoute);
-app.get("/api/config/paypal", (req, res) => {
+app.use('/api/users', userRoute);
+app.use('/api/products', productRoute);
+app.use('/api/orders', orderRoute);
+app.get('/api/config/paypal', (req, res) => {
   res.send(config.PAYPAL_CLIENT_ID);
-})
+});
 
 // app.get("/api/products/:id", (req, res) => {
 //   const productId = req.params.id;
@@ -37,4 +37,4 @@ app.get("/api/config/paypal", (req, res) => {
 //   res.send(data.products);
 // });
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(5000, () => { console.log('Server started at http://localhost:5000'); });
