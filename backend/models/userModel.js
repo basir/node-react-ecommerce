ddb04671e93b813161716fe1f7158c7a8fc3e56a
Basir <basir.jafarzadeh@gmail.com>
2020-03-22 14:10:01 +0430

    update user model unique true

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,11 +2,13 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, dropDups: true },
+  email: {
+    type: String, required: true, unique: true, index: true, dropDups: true,
+  },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, required: true, default: false }
+  isAdmin: { type: Boolean, required: true, default: false },
 });
 
-const userModel = mongoose.model("User", userSchema);
+const userModel = mongoose.model('User', userSchema);
 
 export default userModel;
