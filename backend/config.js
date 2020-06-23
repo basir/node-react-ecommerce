fb210b17b12145d1e81cf0e34bca6783d1712d27
Basir <basir.jafarzadeh@gmail.com>
2020-06-23 22:50:47 +0430

    Video-26-Upload-Product-Image-To-AWS-S3

--- backend/config.js
+++ backend/config.js
@@ -7,4 +7,6 @@ export default {
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+  accessKeyId: process.env.accessKeyId || 'accessKeyId',
+  secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
 };
