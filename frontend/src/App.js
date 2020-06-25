153d0e465d03a76966be06f9e31422a92e6d6a84
mharn <redcap@rocketmail.com>
2020-06-25 10:42:21 +0900

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -85,7 +85,7 @@ function App() {
             <Route path="/" exact={true} component={HomeScreen} />
           </div>
         </main>
-        <footer className="footer">All right reserved.</footer>
+        <footer className="footer">All rights reserved.</footer>
       </div>
     </BrowserRouter>
   );
