88ea042c1a61b3f11770a93a33e2b787e13afafa
Godfrey Turqueza <62551257+godfreyturqz@users.noreply.github.com>
2020-12-09 10:56:25 +0800

    remove redundant imported axios module

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -16,7 +16,6 @@ import {
   PRODUCT_REVIEW_SAVE_SUCCESS,
 } from '../constants/productConstants';
 import axios from 'axios';
-import Axios from 'axios';
 
 const listProducts = (
   category = '',
@@ -46,14 +45,14 @@ const saveProduct = (product) => async (dispatch, getState) => {
       userSignin: { userInfo },
     } = getState();
     if (!product._id) {
-      const { data } = await Axios.post('/api/products', product, {
+      const { data } = await axios.post('/api/products', product, {
         headers: {
           Authorization: 'Bearer ' + userInfo.token,
         },
       });
       dispatch({ type: PRODUCT_SAVE_SUCCESS, payload: data });
     } else {
-      const { data } = await Axios.put(
+      const { data } = await axios.put(
         '/api/products/' + product._id,
         product,
         {
